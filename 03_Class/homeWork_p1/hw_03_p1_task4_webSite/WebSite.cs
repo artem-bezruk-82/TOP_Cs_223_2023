c5bc30646691519bf5b0c2499b3d1fa40e3e58c7
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-27 23:19:56 +0300

    Module#3 home work part#1 task#4 done

---
+++ 03_Class/homeWork_p1/hw_03_p1_task4_webSite/WebSite.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_03_p1_task4_webSite
+{
+    internal class WebSite
+    {
+        public string? Name { get; set; }
+        public Uri? Uri { get; set; }
+        public IPAddress? IPaddress { get; set; }
+        public string? Description { get; set; }
+
+        public override string ToString()
+        {
+            return $"name: {Name}\nURL: {Uri}\nIp address: {IPaddress}\nDescription: {Description}";
+        }
+    }
+}
