52ee62f3861827a5d4a280f71f786b36a4f83854
Artem Bezruk <artem.bezruk@gmail.com>
2023-02-28 18:13:57 +0300

    Module#3 home work part#1 task#5 done

---
+++ 03_Class/homeWork_p1/hw_03_p1_task5_Magazine/MagazineExt.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_03_p1_task5_Magazine
+{
+    internal static class MagazineExt
+    {
+        public static void SetMagazine(this Magazine magazine) 
+        {
+            Console.WriteLine($"Please enter {nameof(magazine)} name");
+            magazine.Name = Console.ReadLine() ?? string.Empty;
+
+            Console.WriteLine($"Please enter {nameof(magazine)} description");
+            magazine.Description = Console.ReadLine() ?? string.Empty;
+
+            Console.WriteLine("Please enter established date, using DD.MM.YYYY format");
+            DateOnly date = DateOnly.Parse(Console.ReadLine() ?? string.Empty);
+
+            magazine.Phone = $"{GetConsoleInput("Please enter 11-digits phone")}";
+
+            Console.WriteLine("Please enter e-mail address");
+            magazine.Email = new System.Net.Mail.MailAddress(Console.ReadLine() ?? string.Empty);
+
+        }
+
+        private static Int64 GetConsoleInput(string requestText)
+        {
+            Int64? value = null;
+
+            while (value is null)
+            {
+                Console.WriteLine(requestText);
+                try
+                {
+                    value = Int64.Parse(Console.ReadLine() ?? "");
+                }
+                catch (Exception exc)
+                {
+
+                    Console.WriteLine($"{exc.Message} Please enter integer value");
+                }
+            }
+            return (Int64)value;
+        }
+    }
+}
---
+++ 03_Class/homeWork_p1/hw_03_p1_task5_Magazine/Program.cs
@@ -0,0 +1,28 @@
+﻿//ДОМАШНЕЕ ЗАДАНИЕ
+
+//Тема: Top level statements. Введение в классы. Обработка исключений
+//Модуль 3. Часть 1
+
+//Задание 5
+//Создайте класс «Журнал». Необходимо хранить в полях класса:
+//название журнала, год основания, описание журнала, контактный телефон, контактный e-mail. 
+//Реализуйте методы класса для ввода данных, вывода данных, реализуйте доступ к отдельным полям через методы класса.
+
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+
+namespace hw_03_p1_task5_Magazine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Magazine magazine = new Magazine();
+            magazine.SetMagazine();
+            Console.WriteLine(magazine);
+
+        }
+    }
+}
